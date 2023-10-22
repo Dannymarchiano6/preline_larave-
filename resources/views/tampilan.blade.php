3e4dcdee9782f82e272a4a80e9126dfe75ebb0d9
Danny Marchiano <98401834+Dannymarchiano6@users.noreply.github.com>
2023-10-22 21:26:17 +0700

    preline laravel

---
+++ resources/views/tampilan.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="./node_modules/preline/dist/preline.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>test preline</title>
+</head>
+<body>
+    <h1>
+        hello world
+    </h1>
+</body>
+</html>
